8de0f28ef9ea6e120584bb37a4891758a02e28fc
KAVATKAR <kavatkar-vishal.prabhakar@capgemini.com>
2022-04-17 21:19:33 +0530

    RemoveAnEmployeeFromPayrollDetails

--- js/home.js
+++ js/home.js
@@ -55,6 +55,18 @@ const createEmployeePayrollJSON = {} => {
     _id: new Date().getTime(),
     _profilePic: '../asset/profile_images/Ellipse -1.png'
   }
-];
+}
 return empPayrollListLocal;
+}
+
+const remove = (node) => {
+  let empPayrollData = empPayrollList.find(empData => empData._id == node.id);
+  if (!empPayrollData) return;
+  const index = empPayrollList
+                 .map(empData => empData._id)
+                 .indexOf(empPayrollData._id);
+  empPayrollList.splice(index, 1);
+  localStorage.setItem("EmployeePayrollList", JSON.stringify(empPayrollList));
+  document.querySelector(".emp-count").textContent = empPayrollList.length;
+  createInnerHtml();
 }
