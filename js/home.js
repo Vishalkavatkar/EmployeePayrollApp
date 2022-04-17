8b38413927f5cc03334cb33813b786fd54b479ec
KAVATKAR <kavatkar-vishal.prabhakar@capgemini.com>
2022-04-17 19:06:55 +0530

    TabularFormatFromJSFileUsingTemplateLiterals

--- js/home.js
+++ js/home.js
@@ -1,18 +1,7 @@
-let employeePayrollDataList;
-
 window.addEventListener('DOMContentLoaded', (event) => {
-  employeePayrollDataList = getEmployeePayrollDataFromStorage();
-  document.querySelector('.emp-count').textContent = employeePayrollDataList.length;
   createInnerHtml();
-  localStorage.removeItem('editEmp');
 });
-
-const getEmployeePayrollDataFromStorage = () => {
-  return localStorage.getItem('EmployeePayrollList') ? JSON.parse(localStorage.getItem('EmployeePayrollList')) : [];
-}
-
 const createInnerHtml = () => {
- 
   const headerHtml = `
     <th></th>
     <th>Name</th>
@@ -20,37 +9,24 @@ const createInnerHtml = () => {
     <th>Department</th>
     <th>Salary</th>
     <th>Start Date</th>
-    <th>Actions</th>`;
+    <th>Actions</th>;
 
-  if(employeePayrollDataList.length == 0)
-    return;
-
-  let innerHtml = `${headerHtml}`;
-
-  for (const employeePayrollData of employeePayrollDataList) {
-
-    innerHtml = `${innerHtml}
+  let empPayrollData = createEmployeePayrollJSON()[0];
+  const innerHtml = ${headerHtml}
       <tr>
-        <td><img class="profile" src="${employeePayrollData._profileImage}" alt=""></td>
-        <td>${employeePayrollData._name}</td>
-        <td>${employeePayrollData._gender}</td>
-        <td>${getDepartmentHtml(employeePayrollData._department)}</td>
-        <td>${employeePayrollData._salary}</td>
-        <td>${employeePayrollData._startDate}</td>
+        <td><img class="profile" src="../asset/profile-images/Ellipse -2.png">
+        <td>Vishal Kavatkar</td>
+        <td>Male</td>
+        <td><div class='dept-lable'>HR</div>
+            <div class='dept-lable'>Finance</div></td>
+        <td>3000000</td>
+        <td>01 NOV 2020</td>
         <td>
-          <img name="${employeePayrollData._id}" src="../assets/icons/delete-black-18dp.svg" alt="delete" id="1" onclick="remove(this)">
-          <img name="${employeePayrollData._id}" src="../assets/icons/create-black-18dp.svg" alt="edit" id="1" onclick="update(this)">
+          <img name="1" onclick="remove(this)" src="../assets/icons/delete-black-18dp.svg">
+          <img name="1" onclick="update(this)" src="../assets/icons/create-black-18dp.svg">
         </td>
       </tr>`;
 
   }
   document.querySelector('#table-display').innerHTML = innerHtml;
-}
-
-const getDepartmentHtml = (departmentList) => {
-  let departmentHtml = '';
-  for (const department of departmentList) {
-    departmentHtml = `${departmentHtml} <div class='dept-label'>${department}</div>`
-  }
-  return departmentHtml
 }
